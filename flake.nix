58f43c5fbad74912ef7d02dae3a7b69ca9de348c
Christoph Waffler <waffler.christoph@gmail.com>
2025-09-13 05:14:38 +0200

    fix: use chrissi as the username

--- flake.nix
+++ flake.nix
@@ -92,8 +92,8 @@
         modules = [
           ./profiles/server.nix
           {
-            home.username = builtins.getEnv "USER";
-            home.homeDirectory = "/home/${builtins.getEnv "USER"}";
+            home.username = "chrissi";
+            home.homeDirectory = "/home/chrissi";
             home.stateVersion = "24.05";
             nixpkgs.config.allowUnfree = true;
           }
