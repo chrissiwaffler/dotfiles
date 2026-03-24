e9ae4f5a85c9e13c5cae14c2dfa2aad3ee2460e0
chrissiwaffler <waffler.christoph@gmail.com>
2026-03-24 23:55:00 +0100

    chore: first omo config

--- modules/desktop/config.nix
+++ modules/desktop/config.nix
@@ -7,5 +7,6 @@
   # Direct xdg config since we're already in home-manager context
   xdg.configFile = {
     "opencode/opencode.jsonc".source = ../../config/opencode/opencode.jsonc;
+    "opencode/oh-my-opencode.jsonc".source = ../../config/opencode/oh-my-opencode.jsonc;
   };
 }
